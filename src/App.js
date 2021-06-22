b8febe5ce297d5e108655832329890b388258294
Ziratsu <enzo.ustariz@hotmail.fr>
2021-06-22 14:28:29 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from "react";
 
 function App() {
+
+  const [formState, setFormState] = useState({
+    name: "",
+    email: ""
+  })  
+
+  const handleInputs = e => {
+
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value
+    })
+
+  }
+
+  // const inputs = useRef([])
+
+  // const addCheck = el => {
+  //   if(el && !inputs.current.includes(el)){
+  //     inputs.current.push(el);
+  //   }
+  // }
+
+ const handleSubmit = e => {
+   e.preventDefault();
+
+   setFormState({
+     name: "",
+     email: ""
+   })
+   // Trigger auth
+  //  console.log(inputs.current, inputs.current[0].value, inputs.current[1].value);
+
+  //  inputs.current.forEach(inp => {
+  //    inp.value = "";
+  //  })
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Your name</label>
+        <input 
+        // ref={addCheck}
+        onChange={handleInputs}
+        value={formState.name}
+        type="text" 
+        id="name" 
+        name="name" 
+        />
+
+        <label htmlFor="email">Your email</label>
+        <input 
+        // ref={addCheck}
+        onChange={handleInputs}
+        value={formState.email}
+        type="email" 
+        id="email" 
+        name="email" 
+        />
+
+        <button>Submit</button>
+      </form>
+
+      <div className="results">
+        <span>Name: {formState.name}</span>
+        <span>Email: {formState.email}</span>
+      </div>
+    </>
   );
 }
 
